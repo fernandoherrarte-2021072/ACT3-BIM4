e303fdd74c8d485d91f6b1a2f53b42e75173bc86
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-09-09 11:11:08 -0600

    feat: Se integraron los componentes el la app principal

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ListaProductos } from './components/lista-productos/lista-productos';
+import { Carrito } from './components/carrito/carrito';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    ListaProductos,
+    Carrito
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+
 }
